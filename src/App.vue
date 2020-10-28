cc0e6a4f08645d3578e874cd1bbe817791acdba8
Markus Bjerkhede <markus.bjerkhede@gmail.com>
2020-10-28 15:35:13 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Mono&display=swap" rel="stylesheet">
+    <div id="nav">
+      <router-link to="/cards">Home</router-link> |
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: 'PT Mono', monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="home">
+    <h1>E-WALLET</h1>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="addNewCard">Add a new card</button>
+  </section>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    addNewCard() {
+      this.$router.push("/addcard");
+    },
+  },
+};
+</script>
+<style lang="scss">
+button {
+  font-size: 22px;
+  font-weight: bold;
+  text-transform: uppercase;
+  border: 2px solid black;
+  padding: 2rem 10rem;
+  border-radius: 8px;
+}
+button:hover {
+  background-color: black;
+  color: white;
+}
+</style>
